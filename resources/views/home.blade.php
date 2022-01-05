c31b053b4298e8f3bb84418949c3875e7f3e19b4
druid <druid@MyDebian>
2022-01-05 16:41:57 +0700

    A-Reliase.0

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,34 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @csrf
         <title>Auth</title>
-
-        <style>
-            .login_container{
-                display: block;
-                width: 20rem;
-            }
-
-            .login_field {
-                display: block;
-                text-align: right;
-            }
-
-            .login_button{
-                display: block;
-                text-align: right;
-            }
-
-        </style>
     </head>
     <body>
-    {{method_field('PATCH')}}
-    {{method_field('PUT')}}
-        <div id="app_login" class="login_container">
-            <div class="login_button">
-                <button onclick="f_enter()">Enter</button>
-                <button onclick="f_logout()">Logout</button>
-                <button onclick="f_register()">Register</button> 
-            </div>
+Здесь ничего нет, все работает через API.
     </body>
     @include('auth.js')
 </html>
--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,3 @@ Route::get('/clear', function() {
 Route::get('/', function () {
     return view('home');
 });
-
-
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/register', function () {
-    return view('auth.register');
-});
